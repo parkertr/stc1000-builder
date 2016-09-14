fc4161755aec36cc595322a545865b8181242243
Troy Parker <parkertr2@gmail.com>
2016-09-15 06:29:57 +1000

    Refactored coffeescript to be data-centric

--- app/coffee/app.coffee
+++ app/coffee/app.coffee
@@ -1,39 +1,106 @@
-@AddStep =
-    add: ->
-        console.log "1. Add next step fields with ramp"
-        elem = $("#addStep-0-0").click((e) ->
-            el = e.target.parent()
-            console.log el
-            el.clone().appendTo el.parent()
-        )
-        console.log "2. Move data from this step to that one"
+class StepOutput
+    out: (step) ->
+        [step.temp(), step.time()]
+    hold: (step) ->
+        [step.temp(), 0]
+    ramp: (fromStep, toStep, degPerMin) ->
+         time = Math.round ((toStep.temp() - fromStep.temp()) / degPerMin)
+         time = Math.max time, 1
+         [fromStep.temp(), time]
+    toObject: (step) ->
+        {'temp':step.temp(),'time':step.time(),'hold':step.hold(),'ramp':step.ramp()}
 
-    init: ->
-        mashStep = $(".stepTemplate")
-        console.log mashStep
-        buttons =  $(".addStepButton")
-        buttons.each (index, button) ->
-            console.log button
-            # console.log button.parent().parent().data
+class Step
+    constructor: (temp, time, ramp=false) ->
+        @_temp = temp
+        @_time = time
+        @_ramp = ramp
+    ramp: ->
+        @_ramp
+    temp: ->
+        @_temp
+    time: ->
+        @_time
+    setRamp: (ramp) ->
+        @_ramp = ramp
+    setTemp: (temp) ->
+        @_temp = temp
+    setTime: (time) ->
+        @_time = time
 
+class Program
+    constructor: (number, output, holdBeforeStart=true, rampDegPerMin=1) ->
+        @_number = number
+        @_output = output
+        @_holdBeforeStart = holdBeforeStart
+        @_rampDegPerMin = rampDegPerMin
+        @_steps = []
+    addStep: (step) ->
+        @_steps.push step
+    setSteps: (steps) ->
+        @_steps = []
+        for step in steps
+            @_steps.push step
+    toArray: ->
+        out = []
+        for step, i in @_steps
+            out.push @_output.hold(step) if @_holdBeforeStart and i is 0
+            out.push @_output.out(step)
+            if (@_steps.length-1) is i
+                out.push @_output.hold(step)
+            else
+                out.push @_output.ramp(step, @_steps[i+1], @_rampDegPerMin) if not step.ramp()
+        out
+    toTemplate: (template) ->
+        out = $('<div/>')
+        for step in @_steps
+            s = template.clone()
+            t = s.find('*[name="temp"]')
+            d = s.find('*[name="duration"]')
 
+            t.val(step.temp())
+            d.val(step.time())
+            t.data('step', step)
+            d.data('step', step)
 
-@doDivThing =
-    do: ->
-        # $("div").click ->
-        #     console.log this
+            t.change (e) =>
+                input = $(e.target)
+                step = input.data('step')
+                step.setTemp input.val()
+                input.data 'step', step
+            d.change (e) =>
+                input = $(e.target)
+                step = input.data('step')
+                step.setTime input.val()
+                input.data 'step', step
 
-programSteps = [
-    [['','',false]], # Program 0, Step 0
-    [['','',false]], # Program 1, Step 0
-    [['','',false]], # Program 2, Step 0
-    [['','',false]], # Program 3, Step 0
-    [['','',false]], # Program 4, Step 0
-    [['','',false]], # Program 5, Step 0
-]
+            s.appendTo out
+        out
+
+Steps =
+    # Add 5 empty programs
+    init: () ->
+        template = $("#stepTemplate")
+
+        output = new StepOutput()
+
+        for i in [0..5]
+            program = new Program(i, output)
+            for step in @default()
+                program.addStep(
+                    new Step(step.temp, step.time, step.hold, step.ramp)
+                )
+            # program.toArray()
+            p = $('[data-program="'+i+'"]')
+            program.toTemplate(template).appendTo p
+
+    default: () ->
+        [
+            {'temp':66.0,'time':60,'hold':false,'ramp':false},
+            {'temp':78.0,'time':10,'hold':false,'ramp':false},
+        ]
 
 jQuery ->
-    AddStep.init()
-    doDivThing.do()
+    Steps.init()
 
     $(document).foundation()
--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,203 @@
 (function() {
-  var programSteps;
+  var Program, Step, StepOutput, Steps;
 
-  this.AddStep = {
-    add: function() {
-      var elem;
-      console.log("1. Add next step fields with ramp");
-      elem = $("#addStep-0-0").click(function(e) {
-        var el;
-        el = e.target.parent();
-        console.log(el);
-        return el.clone().appendTo(el.parent());
-      });
-      return console.log("2. Move data from this step to that one");
-    },
+  StepOutput = (function() {
+    function StepOutput() {}
+
+    StepOutput.prototype.out = function(step) {
+      return [step.temp(), step.time()];
+    };
+
+    StepOutput.prototype.hold = function(step) {
+      return [step.temp(), 0];
+    };
+
+    StepOutput.prototype.ramp = function(fromStep, toStep, degPerMin) {
+      var time;
+      time = Math.round((toStep.temp() - fromStep.temp()) / degPerMin);
+      time = Math.max(time, 1);
+      return [fromStep.temp(), time];
+    };
+
+    StepOutput.prototype.toObject = function(step) {
+      return {
+        'temp': step.temp(),
+        'time': step.time(),
+        'hold': step.hold(),
+        'ramp': step.ramp()
+      };
+    };
+
+    return StepOutput;
+
+  })();
+
+  Step = (function() {
+    function Step(temp, time, ramp) {
+      if (ramp == null) {
+        ramp = false;
+      }
+      this._temp = temp;
+      this._time = time;
+      this._ramp = ramp;
+    }
+
+    Step.prototype.ramp = function() {
+      return this._ramp;
+    };
+
+    Step.prototype.temp = function() {
+      return this._temp;
+    };
+
+    Step.prototype.time = function() {
+      return this._time;
+    };
+
+    Step.prototype.setRamp = function(ramp) {
+      return this._ramp = ramp;
+    };
+
+    Step.prototype.setTemp = function(temp) {
+      return this._temp = temp;
+    };
+
+    Step.prototype.setTime = function(time) {
+      return this._time = time;
+    };
+
+    return Step;
+
+  })();
+
+  Program = (function() {
+    function Program(number, output, holdBeforeStart, rampDegPerMin) {
+      if (holdBeforeStart == null) {
+        holdBeforeStart = true;
+      }
+      if (rampDegPerMin == null) {
+        rampDegPerMin = 1;
+      }
+      this._number = number;
+      this._output = output;
+      this._holdBeforeStart = holdBeforeStart;
+      this._rampDegPerMin = rampDegPerMin;
+      this._steps = [];
+    }
+
+    Program.prototype.addStep = function(step) {
+      return this._steps.push(step);
+    };
+
+    Program.prototype.setSteps = function(steps) {
+      var j, len, results, step;
+      this._steps = [];
+      results = [];
+      for (j = 0, len = steps.length; j < len; j++) {
+        step = steps[j];
+        results.push(this._steps.push(step));
+      }
+      return results;
+    };
+
+    Program.prototype.toArray = function() {
+      var i, j, len, out, ref, step;
+      out = [];
+      ref = this._steps;
+      for (i = j = 0, len = ref.length; j < len; i = ++j) {
+        step = ref[i];
+        if (this._holdBeforeStart && i === 0) {
+          out.push(this._output.hold(step));
+        }
+        out.push(this._output.out(step));
+        if ((this._steps.length - 1) === i) {
+          out.push(this._output.hold(step));
+        } else {
+          if (!step.ramp()) {
+            out.push(this._output.ramp(step, this._steps[i + 1], this._rampDegPerMin));
+          }
+        }
+      }
+      return out;
+    };
+
+    Program.prototype.toTemplate = function(template) {
+      var d, j, len, out, ref, s, step, t;
+      out = $('<div/>');
+      ref = this._steps;
+      for (j = 0, len = ref.length; j < len; j++) {
+        step = ref[j];
+        s = template.clone();
+        t = s.find('*[name="temp"]');
+        d = s.find('*[name="duration"]');
+        t.val(step.temp());
+        d.val(step.time());
+        t.data('step', step);
+        d.data('step', step);
+        t.change((function(_this) {
+          return function(e) {
+            var input;
+            input = $(e.target);
+            step = input.data('step');
+            step.setTemp(input.val());
+            return input.data('step', step);
+          };
+        })(this));
+        d.change((function(_this) {
+          return function(e) {
+            var input;
+            input = $(e.target);
+            step = input.data('step');
+            step.setTime(input.val());
+            return input.data('step', step);
+          };
+        })(this));
+        s.appendTo(out);
+      }
+      return out;
+    };
+
+    return Program;
+
+  })();
+
+  Steps = {
     init: function() {
-      var buttons, mashStep;
-      mashStep = $(".stepTemplate");
-      console.log(mashStep);
-      buttons = $(".addStepButton");
-      return buttons.each(function(index, button) {
-        return console.log(button);
-      });
+      var i, j, k, len, output, p, program, ref, results, step, template;
+      template = $("#stepTemplate");
+      output = new StepOutput();
+      results = [];
+      for (i = j = 0; j <= 5; i = ++j) {
+        program = new Program(i, output);
+        ref = this["default"]();
+        for (k = 0, len = ref.length; k < len; k++) {
+          step = ref[k];
+          program.addStep(new Step(step.temp, step.time, step.hold, step.ramp));
+        }
+        p = $('[data-program="' + i + '"]');
+        results.push(program.toTemplate(template).appendTo(p));
+      }
+      return results;
+    },
+    "default": function() {
+      return [
+        {
+          'temp': 66.0,
+          'time': 60,
+          'hold': false,
+          'ramp': false
+        }, {
+          'temp': 78.0,
+          'time': 10,
+          'hold': false,
+          'ramp': false
+        }
+      ];
     }
   };
 
-  this.doDivThing = {
-    "do": function() {}
-  };
-
-  programSteps = [[['', '', false]], [['', '', false]], [['', '', false]], [['', '', false]], [['', '', false]], [['', '', false]]];
-
   jQuery(function() {
-    AddStep.init();
-    doDivThing["do"]();
+    Steps.init();
     return $(document).foundation();
   });
 
